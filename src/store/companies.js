7004ae8024f2dd735b9e73c7fd3ceccd97131340
richardborgescc <richard.borges.cc@gmail.com>
2019-09-25 16:58:58 -0300

    Codenation. Projeto final: CRUD empresa, falta comunicação com a API.

--- src/store/companies.js
+++ src/store/companies.js
@@ -4,32 +4,66 @@ import { domain } from "env";
 export default {
   namespaced: true,
   state: {
-    companies: []
+    companies: [],
+    company: {}
   },
   actions: {
-    async loadAllCompanies({ commit }) {
+    createCompany({ commit }, form) {
+      commit("CREATE_COMPANY", form);
+    },
+    updateCompany({ commit }, data) {
+      commit("UPDATE_COMPANY", data);
+    },
+    readCompany({ commit }, id) {
+      commit("READ_COMPANY", id);
+    },
+    async readAllCompanies({ commit }) {
       const getCompaniesURL = `${domain}/companies`;
 
       try {
         const { data } = await axios.get(getCompaniesURL);
-        commit("GET_ALL_COMPANIES", data);
+        commit("READ_ALL_COMPANIES", data);
       } catch (error) {
         return error;
+      }
+    },
+    deleteCompany({ commit }, id) {
+      if (confirm("Confirmar exclusão?")) {
+        commit("DELETE_COMPANY", id);
       }
     }
   },
   mutations: {
-    GET_ALL_COMPANIES(state) {
+    CREATE_COMPANY(state, form) {
+      //chamar api
+      alert("CREATE_COMPANY");
+    },
+    READ_COMPANY(state, id) {
+      //chamar api
+      alert("READ_COMPANY");
+    },
+    READ_ALL_COMPANIES(state) {
       state.companies = [
         { id: "1", name: "Empresa 01" },
         { id: "2", name: "Empresa 02" },
         { id: "3", name: "Empresa 03" }
       ];
+    },
+    UPDATE_COMPANY(state, data) {
+      //chamar api
+      alert("UPDATE_COMPANY");
+    },
+    DELETE_COMPANY(state, id) {
+      //chamar api
+      alert("DELETE_COMPANY");
     }
   },
   getters: {
     companies(state) {
       return state.companies;
+    },
+    company(state) {
+      return state.company;
     }
   }
 };

7d9db107982d1ac8d27af050975ebe70f2475a32
richardborgescc <richard.borges.cc@gmail.com>
2019-10-01 15:13:28 -0300

    Codenation. Projeto final: filtro travado ao selecionar opções.

--- src/views/Workspace/LogList.vue
+++ src/views/Workspace/LogList.vue
@@ -9,9 +9,9 @@
                 <font-awesome-icon icon="bug" />
                 <b class="menu-text">{{ log.createdAt }}:</b>
                 <b class="menu-text">{{ log.title }}</b>
-                <span class="menu-text badge badge-secondary">{{
-                  log.levelLog
-                }}</span>
+                <span class="menu-text badge badge-secondary">
+                  {{ log.levelLog }}
+                </span>
               </div>
               <div class="col-3 text-right">
                 <RouterLink
@@ -75,100 +75,107 @@
             <font-awesome-icon icon="filter" />
             <span class="menu-text">Filtros</span>
           </a>
-          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <div class="dropdown-item">
-              <label for="companiesSelect">Empresa</label>
-              <select
-                class="form-control"
-                id="companiesSelect"
-                v-model="form.company"
-                @change="readAllLogs(form)"
-              >
-                <option></option>
-                <option
-                  v-for="company in companies"
-                  :key="company.id"
-                  :value="company.name"
+          <div
+            class="dropup-filter-menu dropdown-menu"
+            onclick=" event.stopPropagation();"
+          >
+            <form class="px-4 py-3">
+              <div class="form-group">
+                <label for="companiesSelect">Empresa</label>
+                <select
+                  class="form-control"
+                  id="companiesSelect"
+                  v-model="form.company"
+                  @change="readAllLogs(form)"
                 >
-                  <span>{{ company.name }}</span>
-                </option>
-              </select>
-            </div>
-            <div class="dropdown-item">
-              <label for="applicationsSelect">Aplicação</label>
-              <select
-                class="form-control"
-                id="applicationsSelect"
-                v-model="form.application"
-                @change="readAllLogs(form)"
-              >
-                <option></option>
-                <option
-                  v-for="application in applications"
-                  :key="application.id"
-                  :value="application.id"
-                  >{{ application.name }}</option
+                  <option></option>
+                  <option
+                    v-for="company in companies"
+                    :key="company.id"
+                    :value="company.name"
+                  >
+                    <span>{{ company.name }}</span>
+                  </option>
+                </select>
+              </div>
+              <div class="form-group">
+                <label for="applicationsSelect">Aplicação</label>
+                <select
+                  class="form-control"
+                  id="applicationsSelect"
+                  v-model="form.application"
+                  @change="readAllLogs(form)"
                 >
-              </select>
-            </div>
-            <div class="dropdown-item">
-              <label for="serverOriginsSelect">Servidor de origem</label>
-              <select
-                class="form-control"
-                id="serverOriginsSelect"
-                v-model="form.serverOrigin"
-                @change="readAllLogs(form)"
-              >
-                <option></option>
-                <option
-                  v-for="serverOrigin in serverOrigins"
-                  :key="serverOrigin.id"
-                  :value="serverOrigin.name"
-                  >{{ serverOrigin.name }}</option
+                  <option></option>
+                  <option
+                    v-for="application in applications"
+                    :key="application.id"
+                    :value="application.id"
+                    >{{ application.name }}</option
+                  >
+                </select>
+              </div>
+              <div class="form-group">
+                <label for="serverOriginsSelect">Servidor de origem</label>
+                <select
+                  class="form-control"
+                  id="serverOriginsSelect"
+                  v-model="form.serverOrigin"
+                  @change="readAllLogs(form)"
                 >
-              </select>
-            </div>
-            <div class="dropdown-item">
-              <label for="levelLogsSelect">Level</label>
-              <select
-                class="form-control"
-                id="levelLogsSelect"
-                v-model="form.levelLog"
-                @change="readAllLogs(form)"
-              >
-                <option></option>
-                <option
-                  v-for="levelLog in levelLogs"
-                  :key="levelLog.id"
-                  :value="levelLog.name"
-                  >{{ levelLog.name }}</option
+                  <option></option>
+                  <option
+                    v-for="serverOrigin in serverOrigins"
+                    :key="serverOrigin.id"
+                    :value="serverOrigin.name"
+                    >{{ serverOrigin.name }}</option
+                  >
+                </select>
+              </div>
+              <div class="form-group">
+                <label for="levelLogsSelect">Level</label>
+                <select
+                  class="form-control"
+                  id="levelLogsSelect"
+                  v-model="form.levelLog"
+                  @change="readAllLogs(form)"
                 >
-              </select>
-            </div>
-            <div class="dropdown-item">
-              <label for="OrderbySelect">Ordenar por</label>
-              <select
-                class="form-control"
-                id="OrderbySelect"
-                v-model="form.orderBy"
-                @change="readAllLogs(form)"
-              >
-                <option>createdAt</option>
-                <option>levelLog</option>
-              </select>
-            </div>
-            <div class="dropdown-item">
-              <input
-                type="checkbox"
-                class="form-check-input"
-                id="exampleCheck1"
-                v-model="form.toFile"
-                @click="readAllLogs(form)"
-              />
-              <label class="form-check-label" for="exampleCheck1"
-                >Arquivado</label
-              >
-            </div>
+                  <option></option>
+                  <option
+                    v-for="levelLog in levelLogs"
+                    :key="levelLog.id"
+                    :value="levelLog.name"
+                    >{{ levelLog.name }}</option
+                  >
+                </select>
+              </div>
+              <div class="form-group">
+                <label for="OrderbySelect">Ordenar por</label>
+                <select
+                  class="form-control"
+                  id="OrderbySelect"
+                  v-model="form.orderBy"
+                  @change="readAllLogs(form)"
+                >
+                  <option>createdAt</option>
+                  <option>levelLog</option>
+                </select>
+              </div>
+              <div class="form-group">
+                <div class="form-check">
+                  <input
+                    type="checkbox"
+                    class="form-check-input"
+                    id="exampleCheck1"
+                    v-model="form.toFile"
+                    @click="readAllLogs(form)"
+                  />
+                  <label class="form-check-label" for="exampleCheck1"
+                    >Arquivado</label
+                  >
+                </div>
+              </div>
+            </form>
           </div>
         </li>
       </ul>
@@ -310,5 +317,9 @@ export default {
 }
 .card-list:hover {
   border-color: #00005181;
+}
+
+.dropup-filter-menu {
+  width: 300px !important;
 }
 </style>

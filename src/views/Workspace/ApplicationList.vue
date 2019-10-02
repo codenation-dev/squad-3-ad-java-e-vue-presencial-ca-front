39e89dd34b39ab3371dcd075ce80c130127ddee9
richardborgescc <richard.borges.cc@gmail.com>
2019-10-02 10:46:52 -0300

    Codenation. Projeto final: Aplicação com componente card-list.

--- src/views/Workspace/ApplicationList.vue
+++ src/views/Workspace/ApplicationList.vue
@@ -1,53 +1,41 @@
 <template>
   <section class="jumbotron main-section">
-    <div
-      class="card card-list"
-      v-for="application in applications"
-      :key="application.id"
-    >
-      <div class="card-header">
-        <div class="w-100">
-          <div class="row">
-            <div class="col-9">
-              <font-awesome-icon icon="desktop" />
-              <b class="menu-text">{{ application.id }}</b>
-            </div>
-            <div class="col-3 card-action-btn-group">
-              <a
-                class="icon-btn card-action-btn"
-                @click="deleteApplication(application.id)"
-                :title="`Excluir ${application.id}`"
-                href="#"
-              >
-                <font-awesome-icon icon="trash-alt" />
-                <span class="card-action-btn-label">Excluir</span>
-              </a>
-              <RouterLink
-                class="icon-btn card-action-btn"
-                :to="{
-                  name: 'application-edit',
-                  params: { id: application.id }
-                }"
-                :title="`Editar ${application.id}`"
-              >
-                <font-awesome-icon icon="pencil-alt" />
-                <span class="card-action-btn-label">Editar</span>
-              </RouterLink>
-            </div>
+    <card-list v-for="application in applications" :key="application.id">
+      <template v-slot:title>
+        <font-awesome-icon icon="desktop" />
+        <b class="menu-text">{{ application.id }}</b>
+      </template>
+      <template v-slot:actions>
+        <a
+          class="icon-btn card-action-btn"
+          @click="deleteApplication(application.id)"
+          :title="`Excluir ${application.id}`"
+          href="#"
+        >
+          <font-awesome-icon icon="trash-alt" />
+          <span class="card-action-btn-label">Excluir</span>
+        </a>
+        <RouterLink
+          class="icon-btn card-action-btn"
+          :to="{
+            name: 'application-edit',
+            params: { id: application.id }
+          }"
+          :title="`Editar ${application.id}`"
+        >
+          <font-awesome-icon icon="pencil-alt" />
+          <span class="card-action-btn-label">Editar</span>
+        </RouterLink>
+      </template>
+      <template v-slot:body>
+        <div class="row">
+          <div class="col-sm">
+            <b>Nome:</b>
+            {{ application.name }}
           </div>
         </div>
-      </div>
-      <div class="card-body">
-        <div class="w-100">
-          <div class="row">
-            <div class="col-sm">
-              <b>Nome:</b>
-              {{ application.name }}
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+      </template>
+    </card-list>
     <BackToLogsButton />
   </section>
 </template>
@@ -55,9 +43,10 @@
 <script>
 import { mapActions, mapGetters } from "vuex";
 import BackToLogsButton from "@/components/Orgs/BackToLogsButton.vue";
+import CardList from "@/components/Orgs/CardList.vue";
 
 export default {
-  components: { BackToLogsButton },
+  components: { CardList, BackToLogsButton },
   computed: {
     ...mapGetters("applications", ["applications"])
   },
